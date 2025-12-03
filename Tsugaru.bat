00201b658694153eaa82cfe0637a410918f0a8bb
Kait Bell <kaitbell521@gmail.com>
2025-12-03 18:09:50 -0500

    replace username with %USERPROFILE%

--- Tsugaru.bat
+++ Tsugaru.bat
@@ -13,9 +13,9 @@ cd ../build
 cmake ../src
 cmake --build . --config Release --target Tsugaru_GUI
 echo Copying to Downloads/Tsugaru ...
-xcopy "..\..\ci_windows\main_cui\Release\Tsugaru_CUI.exe" "C:\Users\kaitb\Downloads\Tsugaru" /Y
-xcopy ..\..\gui\build\main_gui\Release\Tsugaru_GUI.exe C:\Users\kaitb\Downloads\Tsugaru /Y
-xcopy ..\..\gui\build\main_gui\Release\ja.uitxt C:\Users\kaitb\Downloads\Tsugaru /Y
+xcopy "..\..\ci_windows\main_cui\Release\Tsugaru_CUI.exe" "C:\Users\%USERPROFILE%\Downloads\Tsugaru" /Y
+xcopy ..\..\gui\build\main_gui\Release\Tsugaru_GUI.exe C:\Users\%USERPROFILE%\Downloads\Tsugaru /Y
+xcopy ..\..\gui\build\main_gui\Release\ja.uitxt C:\Users\%USERPROFILE%\Downloads\Tsugaru /Y
 echo Copied.
 cd ..\..\..
 explorer "C:\Users\%USERPROFILE%\Downloads\Tsugaru"
