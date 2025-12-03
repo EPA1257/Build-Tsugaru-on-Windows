9c12d96ecf7c80bf07bfeb7998f5bad54cd12958
Kait Bell <kaitbell521@gmail.com>
2025-12-03 18:25:49 -0500

    additions, fixes and remarks
    removed redundant "C:\Users" which was causing an error. added remdir/RD command to delete build folder after files are copied.

--- Tsugaru.bat
+++ Tsugaru.bat
@@ -13,10 +13,14 @@ cd ../build
 cmake ../src
 cmake --build . --config Release --target Tsugaru_GUI
 echo Copying to Downloads/Tsugaru ...
-xcopy "..\..\ci_windows\main_cui\Release\Tsugaru_CUI.exe" "C:\Users\%USERPROFILE%\Downloads\Tsugaru" /Y
-xcopy ..\..\gui\build\main_gui\Release\Tsugaru_GUI.exe C:\Users\%USERPROFILE%\Downloads\Tsugaru /Y
-xcopy ..\..\gui\build\main_gui\Release\ja.uitxt C:\Users\%USERPROFILE%\Downloads\Tsugaru /Y
-echo Copied.
+xcopy /f /y "..\..\ci_windows\main_cui\Release\Tsugaru_CUI.exe" "%USERPROFILE%\Downloads\Tsugaru"
+xcopy /f /y "..\..\gui\build\main_gui\Release\Tsugaru_GUI.exe" "%USERPROFILE%\Downloads\Tsugaru"
+xcopy /f /y "..\..\gui\build\main_gui\Release\ja.uitxt" "%USERPROFILE%\Downloads\Tsugaru"
+rem Edit above line to target your own Tsugaru folder.
+echo Done!
 cd ..\..\..
-explorer "C:\Users\%USERPROFILE%\Downloads\Tsugaru"
+rd /s /q TOWNSEMU
+rem Remove above line if you want to keep the build folder.
+explorer "%USERPROFILE%\Downloads\Tsugaru"
+rem Edit above line to target your own Tsugaru folder. Remove if you don't want your Tsugaru folder to auto-open on completion.
 pause
